085d13534b2e7a99e21354acab43d14f91c81205
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-19 16:01:54 +0300

    <server> model fillables

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'cuisine_id',
+        'title',
+        'description',
+    ];
 }
