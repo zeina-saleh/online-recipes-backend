f5df5880130edd95f1f18fab2f1a5b147ecd7aee
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-19 14:57:16 +0300

    <server> migrations and models

--- database/migrations/2023_08_19_112442_create_recipe_images_table.php
+++ database/migrations/2023_08_19_112442_create_recipe_images_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('recipe_images', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->unsignedBigInteger('recipe_id');
+            $table->string('image_url');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('recipe_images');
     }
 };
