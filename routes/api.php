0b3e7af79a9a6b2cb5b410338eaa41562a9037c2
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-19 15:41:43 +0300

    <server> created cors middleware

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+Route::group(['prefix' => 'auth', 'middleware' => 'cors'], function() {
 Route::post("/login", [AuthController::class, "login"]);
-    Route::post("/register", [AuthController::class, "register"]);
+Route::post("/register", [AuthController::class, "register"]);
+});
