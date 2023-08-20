990c509dfdaef4f50eafb877d27b0ac116415fc8
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-20 11:43:51 +0300

    <server> modified get recipes function

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -14,24 +14,62 @@ use App\Models\IngredientRecipe;
 
 class RecipesController extends Controller
 {
-    function getRecipe($recipeId)
+
+    function getRecipe($recipeId = null)
     {
 
-        $recipe = Recipe::with(['comments', 'likes', 'images', 'cuisine', 'ingredients.unit'])
-            ->find($recipeId);
+        if ($recipeId) {
+            $recipe = Recipe::with(['comments.user', 'likes.user', 'images', 'cuisine', 'ingredients.unit'])
+                ->find($recipeId);
 
-        return response()->json([
-            'recipe' => [
+            $getRecipe = [
                 "id" => $recipe->id,
+                "cuisine" => $recipe->cuisine->name,
                 "title" => $recipe->title,
                 "description" => $recipe->description,
                 "directions" => $recipe->directions,
-            ],
-            'comments' => $recipe->comments,
-            'likes' => $recipe->likes,
-            'images' => $recipe->images,
-            'cuisine' => $recipe->cuisine,
-            'ingredients' => $recipe->ingredients,
-        ]);
+                "created_at" => $recipe->created_at,
+                "updated_at" => $recipe->updated_at,
+                "comments" => $recipe->comments->map(function ($comment) {
+                    return [
+                        "id" => $comment->id,
+                        "user_id" => $comment->user_id,
+                        "recipe_id" => $comment->recipe_id,
+                        "text" => $comment->text,
+                        "created_at" => $comment->created_at,
+                        "updated_at" => $comment->updated_at,
+                        "username" => $comment->user->name,
+                    ];
+                }),
+                "likes" => $recipe->likes->map(function ($like) {
+                    return [
+                        "id" => $like->id,
+                        "user_id" => $like->user_id,
+                        "created_at" => $like->created_at,
+                        "username" => $like->user->name,
+                    ];
+                }),
+                "images" => $recipe->images,
+                "ingredients" => $recipe->ingredients->map(function ($ingredient) {
+                    return [
+                        "id" => $ingredient->id,
+                        "name" => $ingredient->name,
+                        "quantity" => $ingredient->pivot->quantity,
+                        "unit" => $ingredient->unit->name,
+                        "unit_id" => $ingredient->pivot->unit_id,
+                    ];
+                })
+            ];
+
+            return response()->json($getRecipe);
+        } else {
+            $recipes = Recipe::withCount('likes')
+                ->with(['images' => function ($query) {
+                    $query->take(1);
+                }])
+                ->get();
+
+            return response()->json($recipes);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ Route::post("/login", [AuthController::class, "login"]);
 Route::post("/register", [AuthController::class, "register"]);
 Route::get('/logout', [AuthController::class, "logout"]);
 
+
 Route::group(["middleware" => "auth:api"], function () {
-    Route::get('/getRecipe/{recipeId}', [RecipesController::class, 'getRecipe']);
+    Route::get('/getRecipe/{recipeId?}', [RecipesController::class, 'getRecipe']);
 });
