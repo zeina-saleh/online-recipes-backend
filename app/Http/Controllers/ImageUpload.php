f30ed250d2e62ce8197712a34b2f3950eae4a17e
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-21 02:05:18 +0300

    <server> added image upload function

--- app/Http/Controllers/ImageUpload.php
+++ app/Http/Controllers/ImageUpload.php
@@ -8,18 +8,21 @@ class ImageUpload extends Controller
 {
     public function uploadImage(Request $request)
     {
-        /*$request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // adjust the allowed file types and max size as needed
-        ]);*/
+        if ($request->has('images')) {
+            $imageDataArray = $request->input('images'); // Array of base64 image data
 
-        if ($request->has('image')) {
-            $imageData = base64_decode($request->input('image'));
-            $imageName = time() . '.png';
-            $path = storage_path('app/public/images/' . $imageName);
-            file_put_contents($path, $imageData);
-            return response()->json(['message' => 'Image uploaded successfully']);
+            foreach ($imageDataArray as $index => $imageData) {
+                $imageName = time() . "_$index.png"; // Create unique names for images
+                $path = storage_path('app/public/images/' . $imageName);
+
+                // Decode and save each image
+                $decodedImageData = base64_decode($imageData);
+                file_put_contents($path, $decodedImageData);
+            }
+
+            return response()->json(['message' => 'Images uploaded successfully']);
         } else {
-            return response()->json(['message' => 'Image not found'], 400);
+            return response()->json(['message' => 'No images found'], 400);
         }
     }
 }
