47805f9f775810d2fe7cbb1da49f758d170c8fda
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-20 05:11:16 +0300

    <server> formatted recipe api response

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -20,26 +20,18 @@ class RecipesController extends Controller
         $recipe = Recipe::with(['comments', 'likes', 'images', 'cuisine', 'ingredients.unit'])
             ->find($recipeId);
 
-        // Now you can access the related data
-        $recipeTitle = $recipe->title;
-        $recipeComments = $recipe->comments;
-        $recipeLikes = $recipe->likes;
-        $recipeImages = $recipe->images;
-        $recipeCuisine = $recipe->cuisine;
-
-        // Accessing ingredients, quantities, and units
-        $recipeIngredients = $recipe->ingredients;
-
-        foreach ($recipeIngredients as $ingredient) {
-            $ingredientName = $ingredient->name;
-            $pivotData = $ingredient->pivot;
-            $quantity = $pivotData->quantity;
-            $unit = $ingredient->unit->name; // Access the unit name
-            // Access other properties as needed
-        }
-
         return response()->json([
-            'recipe' => $recipe,
+            'recipe' => [
+                "id" => $recipe->id,
+                "title" => $recipe->title,
+                "description" => $recipe->description,
+                "directions" => $recipe->directions,
+            ],
+            'comments' => $recipe->comments,
+            'likes' => $recipe->likes,
+            'images' => $recipe->images,
+            'cuisine' => $recipe->cuisine,
+            'ingredients' => $recipe->ingredients,
         ]);
     }
 }
